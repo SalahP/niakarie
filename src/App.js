623c54fb536d7c0b8bd97158517f693b9b06d7ed
SalahP <hs.salah@outlook.fr>
2019-11-29 15:46:07 +0100

    2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
     return (
       <div class ="container-fluid">
         <Favicon url="./logo.png"></Favicon>
+        <h1 class="text-center text-white">test</h1>
       </div>
 
     )
